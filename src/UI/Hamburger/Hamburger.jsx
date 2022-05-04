90a0bad1c07512fa29473293355ce219f495a3b3
mohamedy72 <mohamedwebdev94@gmail.com>
2022-05-04 14:59:59 +0200

    Implement closing nav menu when changing pages

--- src/UI/Hamburger/Hamburger.jsx
+++ src/UI/Hamburger/Hamburger.jsx
@@ -4,14 +4,21 @@ import { AiOutlineClose } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import "./hamburger.css";
 import useBodyDismiss from "../../hooks/useBodyDismiss";
+import { useLocation } from "react-router-dom";
 
 const Hamburger = ({ className }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const { pathname } = useLocation();
   const toggleMobileNav = () => setIsOpen(!isOpen);
+
   const navRef = useRef(null);
+
   useEffect(() => {
     useBodyDismiss(navRef, setIsOpen);
   }, [navRef]);
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
 
   return (
     <div className="hamburger">
