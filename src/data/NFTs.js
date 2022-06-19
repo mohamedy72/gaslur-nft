2aa4645d8ca3504220f499c26d61a2da53768008
mohamedy72 <mohamedwebdev94@gmail.com>
2022-06-19 17:10:54 +0200

    ..

--- src/data/NFTs.js
+++ src/data/NFTs.js
@@ -1,59 +1,66 @@
 import nft1 from "../assets/imgs/nft1.jpg";
+import nft2 from "../assets/imgs/nft2.jpg";
+import nft3 from "../assets/imgs/nft3.jpg";
+import nft4 from "../assets/imgs/nft4.jpg";
+import nft5 from "../assets/imgs/nft5.jpg";
+import nft6 from "../assets/imgs/nft6.jpg";
+import nft7 from "../assets/imgs/nft7.jpg";
+import nft8 from "../assets/imgs/nft8.jpg";
 
 export const NFTs = [
   {
     id: 1,
     name: "NFT 1",
-    img: "../assets/imgs/nft1.jpg",
+    img: nft1,
     price: 0.2,
     countDown: "01:25:33",
   },
   {
     id: 2,
     name: "NFT 2",
-    img: "./src/assets/imgs/nft2.jpg",
+    img: nft2,
     price: 0.44,
     countDown: "124:25:33",
   },
   {
     id: 3,
     name: "NFT 3",
-    img: "./src/assets/imgs/nft3.jpg",
+    img: nft3,
     price: 1.21,
     countDown: "18:25:33",
   },
   {
     id: 4,
     name: "NFT 4",
-    img: "./src/assets/imgs/nft4.jpg",
+    img: nft4,
     price: 0.32,
     countDown: "12:25:33",
   },
   {
     id: 5,
     name: "NFT 5",
-    img: "./src/assets/imgs/nft5.jpg",
+    img: nft5,
     price: 0.6,
     countDown: "11:25:33",
   },
   {
     id: 6,
     name: "NFT 6",
-    img: "./src/assets/imgs/nft6.jpg",
+    img: nft6,
     price: 0.15,
     countDown: "35:25:33",
   },
   {
     id: 7,
     name: "NFT 7",
-    img: "./src/assets/imgs/nft7.jpg",
+    img: nft7,
     price: 0.22,
     countDown: "44:25:33",
   },
   {
     id: 8,
     name: "NFT 8",
-    img: "./src/assets/imgs/nft8.jpg",
+    img: nft8,
     price: 0.9,
     countDown: "25:25:33",
   },
